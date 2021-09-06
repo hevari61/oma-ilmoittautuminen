ac3478b8e55a2961b3ef7d318130afa140873111
hevari61 <78113850+hevari61@users.noreply.github.com>
2021-09-06 07:39:18 +0300

    dd

--- src/Koti.js
+++ src/Koti.js
@@ -6,7 +6,14 @@ class Koti extends Component {
             <div>
                 <h2>Puulaaki 2021-2022</h2>
                 <p>Katso aiemmat tulokset</p>
-                
+                <p></p>
+                <form>
+  <label>
+    Name:
+    <input type="text" name="Anna nimesi muodossa Sukunimi Etunimi" />
+  </label>
+  <input type="submit" value="Katso" />
+</form>
             </div>
         );
     }
