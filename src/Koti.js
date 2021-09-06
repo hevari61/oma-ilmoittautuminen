e716d5bd46cadcc86460ef5cb319422ef88d0673
hevari61 <78113850+hevari61@users.noreply.github.com>
2021-09-06 06:23:37 +0300

    Lisätty filet
    joskos toimis

---
+++ src/Koti.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Koti extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Puulaaki 2021-2022</h2>
+                <p>Ilmoittaudu seuraavaan kisaan: 08.09.2021</p>
+            </div>
+        );
+    }
+}
+
+export default Koti;
