92f316bbf01cf1d1be49e728748ce7e43a592fad
hevari61 <78113850+hevari61@users.noreply.github.com>
2021-09-14 12:19:50 +0300

    pikkasen täydennetty

--- src/Koti.js
+++ src/Koti.js
@@ -4,8 +4,8 @@ class Koti extends Component {
     render() {
         return (
             <div>
-                <h2>Puulaaki 2021-2022 syyskausi 21  </h2>
-                <p>Ilmoittaudu kilpailuun</p>
+                <h2>Puulaaki 2021-2022 syyskausi 21</h2>
+                <p>Ilmoittaudu kilpailuun tästä</p>
                 <p>Kirjaudu mukaan tästä</p>
                 <form>
   <label>
